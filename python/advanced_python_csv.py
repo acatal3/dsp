004956ef9657310e3ee5d14c594f74417d96c5b8
acatal3 <andrew.catalano91@gmail.com>
2017-01-07 18:11:53 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,9 @@
-PLACE YOUR CODE HERE
+import csv
+from advanced_python_regex import email_addresses
+
+def emails_writer(dataframe):
+    emails_list = email_addresses(dataframe)
+    with open('emails.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for email in emails_list:
+            writer.writerow([email])
